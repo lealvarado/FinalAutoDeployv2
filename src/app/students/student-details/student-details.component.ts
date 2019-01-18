aa930fde187853ff0a37ef194c4c5cb514787954
Leslie Alvarado <lesliemalvarado@gmail.com>
2019-01-18 10:40:33 -0500

    changed scss to css

--- src/app/students/student-details/student-details.component.ts
+++ src/app/students/student-details/student-details.component.ts
@@ -5,7 +5,7 @@ import { StudentService } from '../student.service';
 @Component({
   selector: 'app-student-details',
   templateUrl: './student-details.component.html',
-  styleUrls: ['./student-details.component.scss']
+  styleUrls: ['./student-details.component.css']
 })
 export class StudentDetailsComponent{
   @Input()
